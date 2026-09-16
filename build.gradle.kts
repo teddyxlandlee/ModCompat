7a07350d3096e9bb05810316fc954b9481d24d04
Teddy Li <teddyxlandlee@hotmail.com>
2026-09-16 14:42:40 +0800

    Distinguish compileOnly/runtimeOnly for JarCompat dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ java {
 }
 
 dependencies {
-    implementation("xland.ioutils:JarCompat:0.1.3")
+    compileOnly("xland.ioutils:JarCompat:0.1.3")
+    runtimeOnly("xland.ioutils:JarCompat:0.1.3:all")
 
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
